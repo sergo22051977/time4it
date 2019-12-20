59fc376fef9dd3dbec6dd09b482b29746fcb5b36
Sergey Trubila <sergo22051977@gmail.com>
2019-12-20 20:31:28 +0300

    add: Alert Promt Confirm

---
+++ LESSON_12/scripts/alert-promt-confirm.js
@@ -0,0 +1,8 @@
+'use strict';
+//let x = alert('Alert Promt Confirm');
+//let x = prompt('Сколько тебе лет?')
+//let x = confirm('Ты самый умный?')
+
+//console.log(`Значение x: ${x}`);
+let name = prompt('Как твое имя?');
+console.log(`Привет, ${name}!`);
