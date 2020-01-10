cf6752f8109da6ad882f54581cc31f6ec047c6b0
Sergey Trubila <sergo22051977@gmail.com>
2020-01-10 22:06:26 +0300

    edit: calculator.js learn objects

--- LESSON_13/calculator/calculator.js
+++ LESSON_13/calculator/calculator.js
@@ -1,8 +1,34 @@
 'use strict';
-let x = +prompt('Задайте число X:','0');
-let y = +prompt('Задайте число Y:','0');
+//let x = +prompt('Задайте число X:','0');
+//let y = +prompt('Задайте число Y:','0');
+//
+//function sum(x,y) {
+//  return x + y;
+//}
 
-function mult(x,y) {
-  return x * y
+//function mult(x,y) {
+//  return x * y;
+//}
+
+// END FIRST VERSION
+
+//let user = {};
+//user.name = 'John';
+//user.surname = 'Smith';
+//user.name = 'Pete';
+//delete user.name;
+//
+//function isEmpty(obj) {
+//  for (let key in obj) {
+//    return false;
+//  }
+//  return true;
+//}
+
+function isEmpty(obj) {
+  let sum = 0;
+  for (let key in obj) {
+    sum += obj[key];
+  }
+  return sum;
 }
-
